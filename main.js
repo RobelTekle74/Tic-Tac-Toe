72052a4a80246f911e30ceb46a83936f98f74a91
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2019-01-31 06:11:45 -0500

    front end now has nav bar, home and player dash pages as well as semi-working board.

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// // const path = require('path');
+// // const bodyParser = require('body-parser');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); 
+// // app.use(bodyParser.urlencoded({ extended: false }));
+// // app.use(bodyParser.json());
+// // app.use(express.static(__dirname + '/public'));
+// // // app.set('Views', path.join(__dirname, 'Views'));
+// // // app.set('view engine', 'ejs'); 
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
+// // // // app.get('/', function (req, res) {
+// // // //     res.render('home');
+// // // // });
 
-function run() {
-    app.listen(3000);
-    console.log('Listening on port 3000');
-}
+// // // var a =1 ;
 
-run();
+// // // function dosomething(){
+// // // if(a=1){
+// // //    document.getElementById('').innerHTML='x';
+// // // }else{
+// // //    document.getElementById('').innerHTML='O';
+// // // }
+// // // a=0;
+// // // }
+
+// function run() {
+//     app.listen(3000);
+//     console.log('Listening on port 3000');
+// }
+
+// run();
